26c3797e79c578ed491e5081c4b9add1a2cb14a7
Silver9505 <64332710+Silver9505@users.noreply.github.com>
2020-05-04 19:27:55 -0500

    Create cn.php

---
+++ Tienda_Juegos/Cn/cn.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use GraphAware\Neo4j\Client\ClientBuilder;
+
+$client = ClientBuilder::create()
+
+->addConnection("default", "http://neo4j:12345@localhost:7474")
+
+//Example for HTTP connection configuration (port is optional)
+
+->addConnection("bolt", "bolt://neo4j:12345@localhost:7687")
+
+// Example for BOLT connection configuration (port is optional)
+
+->build();
+
+$client->run('CREATE (sek:Juego{name:"Sekiro™: Shadows Die Twice"})');
+
+?>
